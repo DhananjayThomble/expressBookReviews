a6ad3b05e76209d03c0f18a46f957d0a1a2a89c7
dhananjaythomble <dhananjaythomble@gmail.com>
2023-02-18 02:01:37 -0500

    finish general route

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,32 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    //Write the authenication mechanism here
+    if (!req.headers.authorization) {
+        res.status(401).json({ error: "Unauthorized" });
+    }
+
+    // verify token
+    const token = req.header.authorization.split(" ")[1];
+
+    jwt.verify(token, "fingerprint_customer", (err, decoded) => {
+        if (err) {
+            res.status(401).json({ error: "Invalid Token" });
+
+        } else {
+            req.customer = decoded;
+            next();
+        }
+    })
+
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,26 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    const isFound = users.find( (user) => user.username === username );
+    return !isFound        
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    const user = users.find( user => user.username === username && user.password === password )
+    if (!user) return res.status(401).json({ error: "Invalid Credentials" });
+
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { username, password } = req.body;
+    // check email and password is present
+    if (!username || !password)
+      return res.status(400).json({ error: "Username and password is required" });
+    
+    if( authenticatedUser(username, password) ){
+        //
+    }
 });
 
 // Add a book review
